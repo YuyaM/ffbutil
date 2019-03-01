4b9a30438e597d59e13fab38884786b4a54b0398
YuyaM <miki@iis.u-tokyo.ac.jp>
2019-03-01 17:29:52 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This is the utility program for FrontFlow/Blue(FFB).
+
+you can install it, 'pip3.6 install -e ffbutil'
+
+import ffbutil as fb
+
+ffbutil.history
+ffbutil.samplx
+ffbutil.parmles3x
+ffbutil.naca0012
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ffbutil',
+    version='0.5.0',
+    description='Utility program package for FrontFlow/Blue',
+    long_description=long_description,
+    url='https://github.com/YuyaM/ffbutil',
+    author='YuyaM',
+    author_email='',
+    license='MIT',
+    install_requires=['numpy', 'pandas', 'matplotlib'],
+    keywords='ffbutil',
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
