91f031cbb5dd6e146501a8d7ac6570b1108eece4
YuyaM <miki@iis.u-tokyo.ac.jp>
2019-07-26 18:34:43 +0900

    add histlbm.py

---
+++ ffbutil/histlbm.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##################################################
+# Copyright (c) [2019] [Yuya Miki]
+# 
+# This software is released under the MIT License.
+# http://opensource.org/licenses/mit-license.php
+##################################################
+
+
+class histlbm(history):
+    '''
+    This class is utility for history-file in FrontFlowX
+    '''
+    def __init__(self):
+        '''
+        '''
+        # print("W,H", width, height)
+        self.keyword = {
+            "TIME":       "00",
+        }
+
+        self.keymean= {
+            "TIME":       "TIME                              ",
+        }
+        #
+        # Magic number
+        # この変数はHISTORYファイルを出力する仕様によって決まる
+        # numSolverDefinedHeader : Header
+        # numSolverDefinedValue  : ソルバ定義されたデータ数
+        #
+        numSolverDefinedHeader = 1
+        numSolverDefinedValue = 1
